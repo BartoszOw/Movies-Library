bcb3f0790eb0f5d1c21d1b196a31fed609c53cbb
BartoszOw <owbartosz.pl@gmail.com>
2024-02-20 23:14:43 +0100

    Add instance

--- movie_object.py
+++ movie_object.py
@@ -9,13 +9,11 @@ class Movie:
     def __str__(self):
         return f"{self.title} ({self.prod_date})"
     
-    def play(self, count=1):
-        self._views += count
+    def views(self):
         return self._views
     
-    def __contains__(self, item):
-        return item in self.items
-    
+    def play(self):
+        self._views += 1
 
 
 class Serial(Movie):
@@ -36,7 +34,20 @@ class Serial(Movie):
         
 
 movie_one = Movie(title="Pulp", prod_date=2012, genre="Action")
+movie_two = Movie(title="Meg", prod_date=2000, genre="Horror")
+movie_three = Movie(title="Kevin", prod_date=1999, genre="Comedy")
+movie_four = Movie(title="Beekeeper", prod_date=2024, genre="Action")
+movie_five = Movie(title="Marvel", prod_date=2020, genre="Sci-fi")
+
 serial_one = Serial(title="The Simpsons", prod_date="1999", genre="Comedy", episode=10, season=4)
+serial_two = Serial(title="Breaking Bad", prod_date="2005", genre="Action", episode=20, season=5)
+serial_three = Serial(title="The Walking Dead", prod_date="2010", genre="Horror", episode=25, season=13)
+serial_four = Serial(title="Lucifer", prod_date="1999", genre="Sci-fi", episode=15, season=3)
+serial_five = Serial(title="Bears", prod_date="1999", genre="Documental", episode=5, season=1)
+
 
 print(movie_one)
 print(serial_one)
+movie_one.play()
+movie_one.play()
+print(movie_one.views())
